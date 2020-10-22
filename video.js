d1e5f75381c7e2bd68465ab4290b9a0f48bdb5de
Derek <drubie76@gmail.com>
2020-10-22 11:19:32 -0400

    video update

--- video.js
+++ video.js
@@ -4,17 +4,7 @@
 
 
 var homeVideo = document.getElementById("bVideo");
-function openFullscreen() {
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) { /* Firefox */
-      elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-      elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE/Edge */
-      elem.msRequestFullscreen();
-    }
-  }
+
 
 function playPause(e) {
     var el = document.getElementById("playButton");
@@ -27,7 +17,33 @@ function playPause(e) {
         el.className = "playButton";
     }
 }
+// function openFullscreen() {
+//     if (elem.requestFullscreen) {
+//       elem.requestFullscreen();
+//     } else if (elem.mozRequestFullScreen) { /* Firefox */
+//       elem.mozRequestFullScreen();
+//     } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+//       elem.webkitRequestFullscreen();
+//     } else if (elem.msRequestFullscreen) { /* IE/Edge */
+//       elem.msRequestFullscreen();
+//     }
+//   }
+
+function toggleFullscreen() {
+    let elem = document.querySelector("video");
+  
+    if (!document.fullscreenElement) {
+      elem.requestFullscreen().catch(err => {
+        alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+      });
+    } else {
+      document.exitFullscreen();
+    }
+  }
+
+
+
 homeVideo.addEventListener("click", playPause, false);
-homeVideo.addEventListener("click", openFullscreen, false);
+// homeVideo.addEventListener("click", openFullscreen, false);
 
 
