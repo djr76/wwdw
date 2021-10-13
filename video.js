5c440311140b5e1c05a3df38189b0d763b8350d5
djr76 <53257673+djr76@users.noreply.github.com>
2021-10-13 12:50:31 -0400

    big update

--- video.js
+++ video.js
@@ -1,35 +1,35 @@
 //jshint esversion:6
 
 
-var homeVideo = document.getElementById("bVideo");
+// var homeVideo = document.getElementById("bVideo");
 
 
-function playPause(e) {
-    var el = document.getElementById("playButton");
-    if (homeVideo.paused) {
-        homeVideo.play();
-        el.className = "";
-    } else {
+// function playPause(e) {
+//     var el = document.getElementById("playButton");
+//     if (homeVideo.paused) {
+//         homeVideo.play();
+//         el.className = "";
+//     } else {
 
-        homeVideo.pause();
-        el.className = "playButton";
-    }
-}
+//         homeVideo.pause();
+//         el.className = "playButton";
+//     }
+// }
 
 
-function toggleFullscreen() {
-    let elem = document.querySelector("video");
+// function toggleFullscreen() {
+//     let elem = document.querySelector("video");
 
-    if (!document.fullscreenElement) {
-        elem.requestFullscreen().catch(err => {
-            alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-        });
-    } else {
-        document.exitFullscreen();
-    }
-}
+//     if (!document.fullscreenElement) {
+//         elem.requestFullscreen().catch(err => {
+//             alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+//         });
+//     } else {
+//         document.exitFullscreen();
+//     }
+// }
 
-homeVideo.addEventListener("click", playPause, false);
+// homeVideo.addEventListener("click", playPause, false);
 
 
 
